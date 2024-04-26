8393728359b3e1c1d12f88e676abade116791d36
fill <filatof@gmail.com>
2024-04-26 17:26:50 +0000

    init script

---
+++ create_server_openvpn.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+#------------------------------------------------------------------------
+# Script to Install easy-rsa and openvpn server on Linux Ubuntu or Debian
+#
+# Developed by Ivan Filatoff
+#------------------------------------------------------------------------
+
+# сохраним имя исходного пользователя
+USERNAME="$SUDO_USER"
+
+# Если имя пользователя не определено, выходим с ошибкой
+if [ -z "$USERNAME" ]; then
+    echo "Ошибка: не удалось определить имя пользователя."
+    exit 1
+fi
+
+# Директория, где будет располагаться Easy-RSA
+EASYRSA_DIR="/home/$USERNAME/easy-rsa"
+
+
+# Проверим установлен easy-rs в сситему
+if ! dpkg -s easy-rsa &> /dev/null; then
+	echo "Пакет easy-rsa не установлен. Запустите скрипт create_ca.sh"
+else
+	# Проверяем, существует ли easy-rsa на сервере 
+	if [ -d $EASYRSA_DIR ] && [ -d "$EASYRSA_DIR/pki" ] && [ -f "$EASYRSA_DIR/pki/ca.crt" ] ; then
+        	echo "easy-rsa уже создан"
+	else
+    		# easy-rsa  не существует
+    		# Создаем директорию от пользователя которым запущено sudo
+    		sudo -u "$USERNAME" mkdir -p "$EASYRSA_DIR"
+    
+    		#ограничим доступ к папке 
+    		sudo -u "$USERNAME" chmod 700 "$EASYRSA_DIR"
+    
+   		 #создаем символические ссылки в нашу созданную директорию
+    	 	if ! sudo -u "$USERNAME" ln -s /usr/share/easy-rsa/* "$EASYRSA_DIR"; then  
+     	 		echo "Символические ссылки не созданы"
+         		echo "Проверте наличие директории /usr/share/easy-rsa"
+	 		exit 1
+    		fi
+
+		# Переходим в папку easy-rsa
+		cd "$EASYRSA_DIR" || exit 1
+
+		# Запустим инициализацию PKI
+		if ! sudo -u "$USERNAME" ./easyrsa init-pki ; then
+			echo "Ошибка инициализации. Проверте наличие скрипата easyrsa"
+ 			exit 1
+		fi
+
+		# изменим файл vars заполним своими значениями
+		sudo -u "$USERNAME" cp vars.example vars
+		# Путь к файлу vars
+		VARS_FILE="$EASYRSA_DIR/vars"
+
+		# Заменяемые значения
+		NEW_COUNTRY="RU"
+		NEW_PROVINCE="Moscow"
+		NEW_CITY="Moscow"
+		NEW_ORG="EQ"
+		NEW_EMAIL="admin@example.com"
+		NEW_OU="LLC"
+		NEW_ALGO="ec"
+		NEW_DIGEST="sha512"
+
+		# Проверяем, существует ли файл vars
+		if [ ! -f "$VARS_FILE" ]; then
+    			echo "Файл $VARS_FILE не найден."
+    			exit 1
+		fi
+
+		# Заменяем строки в файле vars
+		sed -i "s/^#set_var EASYRSA_REQ_COUNTRY.*/set_var EASYRSA_REQ_COUNTRY    \"$NEW_COUNTRY\"/" "$VARS_FILE"
+		sed -i "s/^#set_var EASYRSA_REQ_PROVINCE.*/set_var EASYRSA_REQ_PROVINCE    \"$NEW_PROVINCE\"/" "$VARS_FILE"
+		sed -i "s/^#set_var EASYRSA_REQ_CITY.*/set_var EASYRSA_REQ_CITY    \"$NEW_CITY\"/" "$VARS_FILE"
+		sed -i "s/^#set_var EASYRSA_REQ_ORG.*/set_var EASYRSA_REQ_ORG    \"$NEW_ORG\"/" "$VARS_FILE"
+		sed -i "s/^#set_var EASYRSA_REQ_EMAIL.*/set_var EASYRSA_REQ_EMAIL    \"$NEW_EMAIL\"/" "$VARS_FILE"
+		sed -i "s/^#set_var EASYRSA_REQ_OU.*/set_var EASYRSA_REQ_OU    \"$NEW_OU\"/" "$VARS_FILE"
+		sed -i "s/^#set_var EASYRSA_ALGO.*/set_var EASYRSA_ALGO    \"$NEW_ALGO\"/" "$VARS_FILE"
+		sed -i "s/^#set_var EASYRSA_DIGEST.*/set_var EASYRSA_DIGEST    \"$NEW_DIGEST\"/" "$VARS_FILE"
+
+		# запускаем создание СА
+		if ! sudo -u "$USERNAME" ./easyrsa build-ca; then
+			echo "Ошибка при создании CA"
+			exit 1
+		fi
+		echo "Приватный ключ расположен: $EASYRSA_DIR/pki/private/ca.key"
+		echo "Публичный сертификат расположен: $EASYRSA_DIR/pki/ca.crt"
+	fi
+    	echo "Пакет Easy-RSA установлен, продолжим установку..."
+fi
+
+# проверим установку пакета openvpn 
+if ! dpkg -s openvpn &> /dev/null; then
+    echo "Пакет OpenVPN не установлен. Начинаем установку..."
+    
+    # Устанавливаем пакет openvpn
+    sudo apt-get update
+    sudo apt-get install -y openvpn
+    
+    # Проверяем успешность установки
+    if [ $? -eq 0 ]; then
+        echo "OpenVPN успешно установлен."
+    else
+        echo "Ошибка при установке OpenVPN. Пожалуйста, проверьте наличие подключения к интернету и повторите попытку."
+        exit 1
+    fi
+else
+    echo "Пакет OpenVPN уже установлен"
+fi
+
+
+
