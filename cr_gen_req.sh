ae9776dcaf4fed5bb29660ee9a50f5a7c09b1ca1
fill <filatof@gmail.com>
2024-04-29 11:07:40 +0300

    create script cr_gen_req

---
+++ cr_gen_req.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#--------------------------------------------------------------------
+# Script to create request signing  easy-rsa  on Linux
+#
+# Developed by Ivan Filatoff
+#--------------------------------------------------------------------
+
+# сохраним имя исходного пользователя
+USERNAME="$SUDO_USER"
+SERVER=vpnserver
+
+# Если имя пользователя не определено, выходим с ошибкой
+if [ -z "$USERNAME" ]; then
+    echo "Ошибка: не удалось определить имя пользователя."
+    exit 1
+fi
+
+# Директория, где будет располагаться Easy-RSA
+TARGET_DIR="/home/$USERNAME/easy-rsa"
+
+cd $TARGET_DIR || exit 1
+
+./easyrsa gen-req $SERVER nopass
+
+sudo cp /home/sammy/easy-rsa/pki/private/server.key /etc/openvpn/server/
+
+
+
