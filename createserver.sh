73b98613983cf03974bebae4736fa7c91ef2397e
fill <filatof@gmail.com>
2024-05-03 17:59:59 +0000

    add script createserver.sh

---
+++ createserver.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+USERNAME=$1
+HOSTNAME=$2
+META=$3
+
+yc compute instance create \
+  --name $HOSTNAME \
+  --hostname  $HOSTNAME \
+  --zone ru-central1-a \
+  --network-interface subnet-name=my-subnet,nat-ip-version=ipv4 \
+  --preemptible \
+  --create-boot-disk image-folder-id=standard-images,image-family=ubuntu-2204-lts,size=10GB \
+  --platform standard-v1 \
+  --cores 2 \
+  --memory 2GB \
+  --metadata-from-file user-data=$META
