cd7507b156d3a58d3135aea0245f8d766c72f59b
Ivan <filatof@gmail.com>
2024-05-11 12:47:18 +0300

    Update sign_req.sh
    small fix

--- sign_req.sh
+++ sign_req.sh
@@ -4,7 +4,7 @@
 #
 # Developed by Ivan Filatoff
 #------------------------------------------------------------------------
-#Скрипт подписывает запрос на подпись и возвращает файл на сервер VPN
+#Скрипт подписывает запрос на подпись и возвращает файл на сервер VPN, monitor 
 #
 #выбираем для клиента или для сервера подпись
 PARAM=$1
@@ -29,18 +29,25 @@ TARGET_DIR="$HOME/easy-rsa"
 cd "$TARGET_DIR" || exit 1
 
 case $PARAM in
-   server)
+   server) #подпись для сервера
+     # импортируем запрос на подписание
       $TARGET_DIR/easyrsa import-req /home/$USER_CA/$FILE server
       rm /home/$USER_CA/$FILE 
+      # подписываем запрос
       $TARGET_DIR/easyrsa sign-req server server
+      #возвращаем сертификат запросившему подпись серверу
       scp $TARGET_DIR/pki/issued/server.crt $USER_VPN@$IP_SERV_VPN:/home/$USER_VPN
+      #передадим сертификат СА
       scp $TARGET_DIR/pki/ca.crt $USER_VPN@$IP_SERV_VPN:/home/$USER_VPN
       ;;
-   client)
+   client) #подпись для клиента
       client_name=$(basename "$FILE" .req)
+      # импортируем запрос на подпись 
       $TARGET_DIR/easyrsa import-req /home/$USER_CA/$FILE "$client_name"
       rm /home/$USER_CA/$FILE
+      #подписываем запрос
       $TARGET_DIR/easyrsa sign-req client "$client_name"
+      #возвращаем подписаный сертификат
       scp $TARGET_DIR/pki/issued/$client_name.crt $USER_VPN@$IP_SERV_VPN:/home/$USER_VPN/client-configs/keys
       ;;
     *)
