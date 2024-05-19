e8af94c7318ff50f28beb74a1c695251008af8a0
fill <filatof@gmail.com>
2024-05-19 08:33:19 +0000

    fix

--- initvm.sh
+++ initvm.sh
@@ -194,12 +194,12 @@ while true; do
     fi
 
     # скачаем и установим gpg-ключ от репозитория
-    if ! wget --no-check-certificate -P ~/ https://"$repo_login":"$repo_pass"@$REPO.$DOMEN:$repo_port/infra/infra.asc; then
+    if ! sudo -u "$USERNAME" wget --no-check-certificate -P ~/ https://"$repo_login":"$repo_pass"@$REPO.$DOMEN:$repo_port/infra/infra.asc; then
       restore_bkp /etc/apt/sources.list.d/own_repo.list
       restore_bkp /etc/apt/auth.conf.d/auth.conf
       exit 1
     else
-      apt-key add ~/infra.asc
+      gpg --dearmour -o /etc/apt/trusted.gpg.d/infra.gpg /home/$USERNAME/infra.asc
     fi
 
   # скачаем и установим открытый ключ ca-сертификата от репозитория
