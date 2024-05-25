50706fb7c83441b429f23e6dba1c4082323f1650
Ivan <filatof@gmail.com>
2024-05-25 15:12:06 +0300

    Update backup.sh
    fix: изменил порядок архивирования

--- backup.sh
+++ backup.sh
@@ -5,39 +5,23 @@
 # Developed by Ivan Filatoff
 #------------------------------------------------------------------------
 
-
 # Настройки
+SERVER="servername"
 SOURCE_DIR="/home/fill/"                # Путь к директории с данными
-BACKUP_DIR="/home/fill/backup"        # Локальная директория для сохранения архивов
-REMOTE_DIR="/home/fill/backup_infra"       # Директория на резервном сервере
+REMOTE_USER="fill"                        # Пользователь на резервном сервере
 REMOTE_SERVER="192.168.0.6"             # Имя или IP адрес резервного сервера
+REMOTE_DIR="/tmp/backup"       # Директория на резервном сервере для временного хранения данных
+ARCHIVE_DIR="/home/fill/backup_infra"            # Директория на резервном сервере для сохранения архивов
 
-# Удаление локальных архивов старше одного месяца
-find ${BACKUP_DIR} -type f -name "backup_*.tar.gz" -mtime +30 -exec rm -f {} \;
-
-# Проверка успешности удаления старых архивов
-if [ $? -ne 0 ]; then
-  echo "Удаление старых архивов не удалось!"
-  exit 1
-fi
 
 # Получение текущей даты и времени
 CURRENT_DATE=$(date +"%Y%m%d_%H%M%S")
 
 # Имя архива
-ARCHIVE_NAME="backup_${CURRENT_DATE}.tar.gz"
+ARCHIVE_NAME="backup_${SERVER}_${CURRENT_DATE}.tar.gz"
 
-# Архивирование данных
-tar -cvzf ${BACKUP_DIR}/${ARCHIVE_NAME} -C ${SOURCE_DIR} .
-
-# Проверка успешности архивирования
-if [ $? -ne 0 ]; then
-  echo "Архивирование не удалось!"
-  exit 1
-fi
-
-# Отправка архива на резервный сервер
-rsync -avz --delete ${BACKUP_DIR}/${ARCHIVE_NAME} ${REMOTE_SERVER}:${REMOTE_DIR}
+# Передача данных на резервный сервер
+rsync -avz --delete ${SOURCE_DIR} ${REMOTE_USER}@${REMOTE_SERVER}:${REMOTE_DIR}
 
 # Проверка успешности передачи данных
 if [ $? -ne 0 ]; then
@@ -45,6 +29,12 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
+# Архивирование данных на резервном сервере
+ssh ${REMOTE_USER}@${REMOTE_SERVER} "tar -cvzf ${ARCHIVE_DIR}/${ARCHIVE_NAME} -C ${REMOTE_DIR} . && rm -rf ${REMOTE_DIR}/*"
+
+# Проверка успешности архивирования и очистки
+if [ $? -ne 0 ]; then
+  echo "Архивирование данных на резервном сервере не удалось!"
+  exit 1
+fi
 echo "Резервное копирование завершено успешно!"
-
-
