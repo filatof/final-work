7c4ff1ae14a8296c1d2eec283b51edee6c97d94d
filatof <filatof@gmail.com>
2024-05-25 19:27:35 +0300

    fix backup

--- backup.sh
+++ backup.sh
@@ -6,9 +6,9 @@
 #------------------------------------------------------------------------
 
 # Настройки
-SERVER="servername"
+SERVER="monitor"
 # Массив директорий с данными
-SOURCE_DIR=("/home/fill/" "/etc/openvpn/server" "/opt/openvpn_exporter")                
+SOURCE_DIRS=("/home/fill/" "/etc/prometheus" "/opt/node_exporter")                
 REMOTE_USER="fill"                        # Пользователь на резервном сервере
 REMOTE_SERVER="192.168.0.6"             # Имя или IP адрес резервного сервера
 REMOTE_DIR="/tmp/backup"       # Директория на резервном сервере для временного хранения данных
@@ -21,21 +21,35 @@ CURRENT_DATE=$(date +"%Y%m%d_%H%M%S")
 # Имя архива
 ARCHIVE_NAME="backup_${SERVER}_${CURRENT_DATE}.tar.gz"
 
+
+echo "Начало резервного копирования..."
+
 # Передача данных на резервный сервер
-rsync -avz --delete ${SOURCE_DIR} ${REMOTE_USER}@${REMOTE_SERVER}:${REMOTE_DIR}
+for DIR in "${SOURCE_DIRS[@]}"; do
+  BASENAME=$(basename ${DIR})
+  REMOTE_PATH="${REMOTE_DIR}/${BASENAME}_${SERVER}"
 
-# Проверка успешности передачи данных
-if [ $? -ne 0 ]; then
-  echo "Передача данных на резервный сервер не удалась!"
-  exit 1
-fi
+  echo "Создание директории на удаленном сервере: ${REMOTE_PATH}"
+  ssh ${REMOTE_USER}@${REMOTE_SERVER} "mkdir -p ${REMOTE_PATH}"
+  if [ $? -ne 0 ]; then
+    echo "Не удалось создать директорию на удаленном сервере: ${REMOTE_PATH}"
+    exit 1
+  fi
 
-# Архивирование данных на резервном сервере
-ssh ${REMOTE_USER}@${REMOTE_SERVER} "tar -cvzf ${ARCHIVE_DIR}/${ARCHIVE_NAME} -C ${REMOTE_DIR} . && rm -rf ${REMOTE_DIR}/*"
+  echo "Передача данных из ${DIR} в ${REMOTE_PATH}"
+  rsync -avz --delete "${DIR}/" "${REMOTE_USER}@${REMOTE_SERVER}:${REMOTE_PATH}"
+  if [ $? -ne 0 ]; then
+    echo "Передача данных из ${DIR} на резервный сервер не удалась!"
+    exit 1
+  fi
+done
 
-# Проверка успешности архивирования и очистки
+echo "Архивирование данных на резервном сервере в файл ${ARCHIVE_DIR}/${ARCHIVE_NAME}"
+ssh ${REMOTE_USER}@${REMOTE_SERVER} "tar -cvzf ${ARCHIVE_DIR}/${ARCHIVE_NAME} -C ${REMOTE_DIR} ."
+
 if [ $? -ne 0 ]; then
   echo "Архивирование данных на резервном сервере не удалось!"
   exit 1
 fi
-echo "Резервное копирование завершено успешно!"
+
+echo "Резервное копирование и архивирование завершено успешно!"
